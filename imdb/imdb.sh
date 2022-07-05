f20de4a6e80cd4a8ee8e17f61a5d8e69e9a56bc4
milankomaj <milankomaj+Github@gmail.com>
2022-07-05 15:49:31 +0200

    imdb

--- imdb/imdb.sh
+++ imdb/imdb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -s https://datasets.imdbws.com/title.ratings.tsv.gz | gunzip >> data.tsv
-awk 'length($3) >= 6' data.tsv > numVotes_6+.csv 
-awk '($2) >= 9.0' numVotes_6+.csv >  numVotes_6+averageRating_9+.csv 
-awk 'length($3) >= 5&&($2) >= 9.5' data.tsv > imdb.csv
-wc -l imdb.csv
+awk 'length($3) >= 6' data.tsv > numVotes_6+.tsv 
+awk '($2) >= 9.0' numVotes_6+.tsv >  numVotes_6+averageRating_9+.tsv 
+awk 'length($3) >= 5&&($2) >= 9.5' data.tsv > imdb.tsv
+wc -l imdb.tsv
 
