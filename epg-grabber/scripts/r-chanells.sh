25a2dabd450ed5a2065a15f38de226a10f69b1ba
milankomaj <milankomaj+Github@gmail.com>
2023-02-06 20:24:41 +0100

    r-chanells

---
+++ epg-grabber/scripts/r-chanells.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#
+for Rchanells in {r-horizon,r-o2,r-mujtvprogram}
+do
+echo "${Rchanells}"
+
+curl -X GET \
+  --connect-timeout 20 \
+  --max-time 20 \
+  --no-progress-meter \
+  --url ${{ vars.SHEET_ELEONORA }}${Rchanells} \
+| jq '.[] | del(.prefix)  | join(",")'  | tr -d $'\r' | sort -t, -k3  > ${Rchanells}.tsv
+
+sed -i 's/\"//g' ${Rchanells}.tsv
+sed -i 's/&/and/g' ${Rchanells}.tsv
+sed -i '/^[[:space:]]*$/d' ${Rchanells}.tsv
+awk -F ',' '{print "    <channel lang=\"sk\" xmltv_id=\""$1"\""" site_id=\""$2"\""" logo=\""$5"\""">"$3"</channel>"}' ${Rchanells}.tsv > channels/${Rchanells}.xml
+sed -i 1i"<?xml version=\"1.0\" encoding=\"UTF-8\"?>" channels/${Rchanells}.xml
+sed -i 2i"<site site=\"${Rchanells}.tv\">" channels/${Rchanells}.xml
+sed -i 3i"\ \ <channels>" channels/${Rchanells}.xml
+echo "  </channels>" >> channels/${Rchanells}.xml
+echo "</site>" >> channels/${Rchanells}.xml
+sed 1i"xmltv_id,site_id,name,channelNumber,logoUrl" ${Rchanells}.tsv > csv/${Rchanells}.csv
+done
