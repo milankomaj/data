df9862454d3d552cdb06a217c7885615c9a57386
milankomaj <82147606+milankomaj@users.noreply.github.com>
2023-04-21 20:17:03 +0200

    Update o2_tv_list_curl.sh

--- epg-grabber/scripts/o2_tv_list_curl.sh
+++ epg-grabber/scripts/o2_tv_list_curl.sh
@@ -6,6 +6,17 @@ curl -X GET \
   --connect-timeout 30 \
   --max-time 30 \
   --url "https://api.o2tv.cz/unity/api/v1/epg/depr/?forceLimit=true&limit=500" \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/jxl,image/webp,*/*;q=0.8' \
+  -H 'Accept-Language: sk,en-US;q=0.7,en;q=0.3' \
+  -H 'Accept-Encoding: gzip, deflate, br' \
+  -H 'DNT: 1' \
+  -H 'Connection: keep-alive' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: cross-site' \
+  -H 'Pragma: no-cache' \ 
+  -H 'Cache-Control: no-cache' \
 | jq -r '.epg.items[] | ["o2-\(.channel.weight)", .channel.channelKey, .channel.name, .channel.weight, "https://assets.o2tv.cz\(.channel.logoUrl)"] | join(",")' | sort -t, -k3 > csv/o2_chanell.csv
 #
 awk -F ',' '{print "    <channel lang=\"cz\" xmltv_id=\""$1"\""" site_id=\""$2"\""" logo=\""$5"\""">"$3"</channel>"}' csv/o2_chanell.csv  > channels/o2_chanell_.xml
