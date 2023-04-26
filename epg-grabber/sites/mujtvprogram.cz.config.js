96985cb1324eeb213aca264653fd94288ea35d66
milankomaj <milankomaj+Github@gmail.com>
2023-04-26 14:54:34 +0200

    return []

--- epg-grabber/sites/mujtvprogram.cz.config.js
+++ epg-grabber/sites/mujtvprogram.cz.config.js
@@ -79,7 +79,7 @@ function parseCategory(item) {
 }
 
 function parseIcon(item) {
-  if (!item) return null
+  if (!item) return []
   // console.log("parseIcon",item[0].url._text)
   return item[0].url._text
 }
