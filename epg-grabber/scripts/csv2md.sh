4bce38a97b6721ea7145383f1e4dd30c6950f111
milankomaj <milankomaj+Github@gmail.com>
2024-04-09 23:19:23 +0200

    csv2md

--- epg-grabber/scripts/csv2md.sh
+++ epg-grabber/scripts/csv2md.sh
@@ -3,7 +3,6 @@
 # file=$(ls *.csv)
 echo -e "\033[31;1m csv2md \033[0m"
 
-
 for chanells in {horizon.tv_sk.chanell,mujtvprogram.channels,programandroid.channels,solocoo.tv_cz.chanell,solocoo.tv_sk.chanell}
 do
 echo "${chanells}"
