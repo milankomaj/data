68c9aa1a6fa519e8e4d8ff7823091c994a0ae408
milankomaj <milankomaj+Github@gmail.com>
2024-04-09 22:37:42 +0200

    csv2md

---
+++ epg-grabber/scripts/csv2md.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# file=$(ls *.csv)
+echo -e "\033[31;1m csv2md \033[0m"
+for chanells in ${horizon.tv_sk.chanell.csv,mujtvprogram.channels.csv,programandroid.channels.csv,solocoo.tv_cz.chanell.csv,solocoo.tv_sk.chanell.csv}
+do
+echo "${chanells}"
+awk -F ',' '{print "![]("$5") | **"$3"** | "$4}' csv/${chanells} > md/${chanells}.md
+sed -i 2i":---: | :---: | :---:" md/${chanells}.md
+done
+echo "done"
