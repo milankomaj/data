80ca45a1f3f41114011ac8e85adb85721ab40c26
milankomaj <milankomaj+Github@gmail.com>
2025-05-04 05:30:10 +0200

    cookies

--- playwright/tests/solocoo.spec.js
+++ playwright/tests/solocoo.spec.js
@@ -4,7 +4,9 @@ const { test, expect } = require('@playwright/test');
 
 test('CZ', async ({ page }) => {
   await page.goto('https://livetv.skylink.cz');
-  // await expect(page.getByText('Prohlédnout')).toBeVisible();
+  await page.getByRole('link', { name: 'Pouze nutné cookies' }).click();
+
+  await expect(page.getByText('Prohlédnout')).toBeVisible();
   await expect(page).toHaveTitle(/Skylink/);
   // await page.screenshot({ path: `test-results/title.png` });
 
@@ -24,7 +26,9 @@ test('CZ', async ({ page }) => {
 
 test('SK', async ({ page }) => {
   await page.goto('https://livetv.skylink.sk');
-  // await expect(page.getByText('Pozrieť')).toBeVisible();
+  await page.getByRole('link', { name: 'Len potrebné cookies' }).click();
+
+  await expect(page.getByText('Pozrieť')).toBeVisible();
   await expect(page).toHaveTitle(/Skylink/);
   // await page.screenshot({ path: `test-results/title.png` });
 
